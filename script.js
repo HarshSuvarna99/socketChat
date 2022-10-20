029b7f7be86da80cb65f068b902ea0854f289a0f
HarshSuvarna <harsh.suvarna0@gmail.com>
2022-10-21 00:45:10 +0530

    personal messages

--- script.js
+++ script.js
@@ -2,32 +2,44 @@ const socket = io('http://localhost:3000')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
+const messageTo = document.getElementById('message_to')
+
 
 const name = prompt('What is your name?')
 appendMessage('You joined')
 socket.emit('new-user', name)
 
+//receiving mssg
+//to display incoming messages on screen
 socket.on('chat-message', data => {
   appendMessage(`${data.name}: ${data.message}`)
 })
 
+//to display when user gets connected
 socket.on('user-connected', name => {
   appendMessage(`${name} connected`)
 })
 
+//to display when user gets disconnected
 socket.on('user-disconnected', name => {
   appendMessage(`${name} disconnected`)
 })
 
+//sending messsage
 messageForm.addEventListener('submit', e => {
   e.preventDefault()
   const message = messageInput.value
+  const sendTo = messageTo.value
+  //show self message to the client
   appendMessage(`You: ${message}`)
-  socket.emit('send-chat-message', message)
+  //send message 
+  socket.emit('send-chat-message', message, sendTo)
   messageInput.value = ''
+  messageTo.value = ''
 })
 
 function appendMessage(message) {
+  //creating a new div for the message for the sent message to show in the display
   const messageElement = document.createElement('div')
   messageElement.innerText = message
   messageContainer.append(messageElement)
--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-const io = require('socket.io')(3000)
+const io = require('socket.io')(3000, {
+  cors:{
+    origin:["http://localhost:5500"]
+  }
+})
 
 const users = {}
 
 io.on('connection', socket => {
+  //give ids to every new client connected 
   socket.on('new-user', name => {
     users[socket.id] = name
+    console.log(users);
     socket.broadcast.emit('user-connected', name)
   })
-  socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+
+  //receiving mesaage from the client and sending it to everyone who is connected
+  socket.on('send-chat-message', (message, room) => {
+    if (room) {
+      //send personal message
+      socket.to(room).emit('chat-message', {message:message, name:users[socket.id]})
+    } else {
+      socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    }
+    
   })
   socket.on('disconnect', () => {
     socket.broadcast.emit('user-disconnected', users[socket.id])
