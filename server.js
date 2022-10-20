caa748b6a5daf6d1f92ca20c214a72a391fe7b41
Harsh_Suvarna <harsh.suvarna@codeb.online>
2022-10-20 11:49:22 +0530

    secondcommit

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
-const httpServer = 3000
+const io = require('socket.io')(3000)
 
-const io = require("socket.io")(3000
-  , {
-  cors: {
-    origin: "https://localhost:3000",
-    methods: ["GET", "POST"]
-  }
-});
 const users = {}
 
 io.on('connection', socket => {
